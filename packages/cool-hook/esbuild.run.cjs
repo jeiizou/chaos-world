b44b13131fdfd71336bdcf4315f8f2484cbaffce
zhoujie.x <zhoujie.x@bytedance.com>
2022-08-28 11:05:17 +0800

    feat: update build process

--- packages/cool-hook/esbuild.run.cjs
+++ packages/cool-hook/esbuild.run.cjs
@@ -3,14 +3,15 @@ const path = require('path');
 const entryPoints = path.resolve(__dirname, './src/index.ts');
 const outfile = path.resolve(__dirname, 'dist/index.js');
 
-let res = require('esbuild').buildSync({
+const buildResult = require('esbuild').buildSync({
     entryPoints: [entryPoints],
     outfile: outfile,
     bundle: true,
     external: ['react'],
     format: 'esm',
+    minify: true,
 });
 
-if (res.errors.length === 0) {
+if (buildResult.errors.length === 0) {
     console.log('build success');
 }
