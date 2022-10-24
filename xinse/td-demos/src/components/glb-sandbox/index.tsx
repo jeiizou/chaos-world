9aba23d2b3923d7e552dea757cad3059c96d7f8a
zhoujie.x <zhoujie.x@bytedance.com>
2022-10-24 19:42:55 +0800

    feat: glb loader

--- xinse/td-demos/src/components/glb-sandbox/index.tsx
+++ xinse/td-demos/src/components/glb-sandbox/index.tsx
@@ -1,99 +1,125 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   Scene,
-  Clock,
   WebGLRenderer,
   sRGBEncoding,
-  PMREMGenerator,
-  Color,
+  AmbientLight,
   PerspectiveCamera,
-  AnimationMixer
+  DirectionalLight,
+  PCFSoftShadowMap,
+  ACESFilmicToneMapping,
+  SpotLight
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment';
 
-// import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-import aModel from '../../assets/models/a.gltf';
+function loadModel(onLoading) {
+  const container = document.getElementById('container');
+
+  if (!container) return;
+
+  const renderer = new WebGLRenderer({ antialias: true });
+
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.outputEncoding = sRGBEncoding;
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = PCFSoftShadowMap;
+  renderer.toneMapping = ACESFilmicToneMapping;
+  container.appendChild(renderer.domElement);
+
+  const scene = new Scene();
+
+  scene.add(new AmbientLight(0x443333));
+
+  const spotLight = new SpotLight(0xffffff, 10);
+  spotLight.position.set(25, 50, 25);
+  spotLight.angle = Math.PI / 6;
+  spotLight.penumbra = 1;
+  spotLight.decay = 2;
+  spotLight.distance = 100;
+  scene.add(spotLight);
+
+  const camera = new PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 100);
+  camera.position.set(5, 2, 8);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 0.5, 0);
+  controls.update();
+  controls.enablePan = false;
+  controls.enableDamping = true;
+
+  const loader = new GLTFLoader();
+
+  const dracoLoader = new DRACOLoader();
+
+  dracoLoader.setDecoderConfig({ type: 'js' });
+  dracoLoader.setDecoderPath('https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/js/libs/draco/');
+
+  loader.setDRACOLoader(dracoLoader);
+
+  console.log('start load');
+  const time = new Date().getTime();
+  loader.load(
+    '/dog.glb',
+    function (gltf) {
+      const loadedTime = new Date().getTime() - time;
+      console.log('rendered', `cost ${loadedTime / 1000}s`);
+      const model = gltf.scene;
+      model.position.set(0, 0, 0);
+      model.scale.set(0.1, 0.1, 0.1);
+      scene.add(model);
+
+      animate();
+    },
+    (xhr) => {
+      onLoading((xhr.loaded / xhr.total) * 100);
+    },
+    function (e) {
+      console.error(e);
+    }
+  );
+
+  window.onresize = function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  };
+
+  function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+
+    const time = performance.now() / 1000;
+    spotLight.position.x = Math.cos(time) * 45;
+    spotLight.position.z = Math.sin(time) * 45;
+
+    renderer.render(scene, camera);
+  }
+}
 
 export default function GlbSandBox(): React.ReactElement {
+  const init = useRef(false);
+  const [progress, setProgress] = useState(0);
+
   useEffect(() => {
-    let mixer: AnimationMixer;
-
-    const clock = new Clock();
-    const container = document.getElementById('container');
-
-    if (!container) return;
-
-    const renderer = new WebGLRenderer({ antialias: true });
-
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.outputEncoding = sRGBEncoding;
-    container.appendChild(renderer.domElement);
-
-    const pmremGenerator = new PMREMGenerator(renderer);
-
-    const scene = new Scene();
-    scene.background = new Color(0xbfe3dd);
-    scene.environment = pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture;
-
-    const camera = new PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 100);
-    camera.position.set(5, 2, 8);
-
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.target.set(0, 0.5, 0);
-    controls.update();
-    controls.enablePan = false;
-    controls.enableDamping = true;
-
-    // const dracoLoader = new DRACOLoader();
-    // dracoLoader.setDecoderPath('js/libs/draco/gltf/');
-
-    const loader = new GLTFLoader();
-    // loader.setDRACOLoader(dracoLoader);
-
-    console.log('start load');
-    loader.load(
-      aModel,
-      function (gltf) {
-        console.log('loaded');
-        const model = gltf.scene;
-        model.position.set(1, 1, 0);
-        model.scale.set(0.01, 0.01, 0.01);
-        scene.add(model);
-
-        mixer = new AnimationMixer(model);
-        mixer.clipAction(gltf.animations[0]).play();
-
-        animate();
-      },
-      undefined,
-      function (e) {
-        console.error(e);
-      }
-    );
-
-    window.onresize = function () {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-
-      renderer.setSize(window.innerWidth, window.innerHeight);
-    };
-
-    function animate() {
-      requestAnimationFrame(animate);
-
-      const delta = clock.getDelta();
-
-      mixer.update(delta);
-
-      controls.update();
-
-      renderer.render(scene, camera);
+    if (!init.current) {
+      loadModel((v) => {
+        setProgress(v);
+      });
+      init.current = true;
     }
   }, []);
 
-  return <div id="container"></div>;
+  return (
+    <>
+      <div id="container"></div>
+      <div style={{ position: 'absolute', top: 0, display: progress >= 100 ? 'none' : 'block' }}>
+        loading{progress.toFixed(2)} %
+      </div>
+    </>
+  );
 }
